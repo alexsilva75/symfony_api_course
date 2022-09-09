f93dabb8e4b8bf434519f7ccc027098f01d35f05
Alexsandro Silva Santos <alexsilva75>
2022-09-09 20:09:40 -0300

    Saving and retrieving data to/from the database

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,38 +8,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/blog', name: 'blog_')]
 class BlogController extends AbstractController
 {
     
-    #[Route('/{page}',  name: 'blog_list', defaults: ['page' => 1], methods: ['GET'])]
-    public function list($page){
+    #[Route('/{page}',  name: 'blog_list', defaults: ['page' => 1], methods: ['GET'], requirements: ['page' => "\d+"])]
+    public function list($page, Request $request, BlogPostRepository $blogPostRepository){
+        $limit = $request->get('limit', 10);
+        $blogPosts = $blogPostRepository->findAll();
+
+        $blogPostsUrls = array_map(function(BlogPost $post){
+            return $this->generateUrl('blog_by_slug', ['slug' => $post->getSlug()]);
+        },$blogPosts);
+
         return $this->json([
             'page' => $page,
+            'limit' => $limit,
+            'blog_posts_urls' => $blogPostsUrls,
             
         ]);
     }
 
     #[Route('/post/{id}', name: 'blog_by_id', requirements: ['id' => "\d+"], methods: ['GET']  )]
-    public function post(){
+    public function post($id, BlogPostRepository $blogPostRepository){
         return $this->json([
-            'message' => 'Welcome to your new BLOG BY ID controller!',
-            'path' => 'src/Controller/BlogController.php',
+            'blog_post' => $blogPostRepository->find($id),
+          
         ]);
     }
 
-    #[Route('/post/{slug}', name: 'blog_by_slug', methods: ['GET'])]
-    public function postBySlug(){
+    #[Route('/post/{slug}', name: 'by_slug', methods: ['GET'])]
+    public function postBySlug($slug, BlogPostRepository $blogPostRepository){
         return $this->json([
-            'message' => 'Welcome to your new  BLOG BY SLUG controller!',
-            'path' => 'src/Controller/BlogController.php',
+            'blog_post' => $blogPostRepository->findOneBy(['slug' => $slug]),
+            
         ]);
     }
 
     #[Route('/add', name: 'blog_add', methods:['POST'])]
-    public function add(Request $request, Serializer $serializer, BlogPostRepository $blogPostRepository){
+    public function add(Request $request, SerializerInterface $serializer, BlogPostRepository $blogPostRepository){
         $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
 
         //dd($blogPost);
